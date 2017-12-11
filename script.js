a5585ef42ccc580b3fd7a1217e0eb7323b801544
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2017-12-11 12:08:30 -0700

    dfgaw

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ $("#winter").click(setWinter);
 
 function setSpring()  {
   $("body").css("background-image", "url('images/springblur.jpg')");
-  $("#sprpic").attr("src", "images/spring.jpg");
+  $("img").attr("src", "images/spring.jpg");
+  $("h1").text("Spring");
+  $("a").attr("href", "https://www.youtube.com/watch?v=mFWQgxXM_b8").attr("target", "_blank");
 }
 
 function setSummer()  {
   $("body").css("background-image", "url('images/summerblur.jpg')");
-  $("#sumpic").attr("src", "images/summer.jpg");
+  $("img").attr("src", "images/summer.jpg");
+  $("h1").text("Summer");
+  $("a").attr("href", "https://www.youtube.com/watch?v=VC3qO2V1AXY").attr("target", "_blank");
 }
 
 function setAutumn()  {
   $("body").css("background-image", "url('images/autumnblur.jpg')");
-  $("#autpic").attr("src", "images/autumn.jpg");
+  $("img").attr("src", "images/autumn.jpg");
+  $("h1").text("Autumn");
+  $("a").attr("href", "https://www.youtube.com/watch?v=8Q8ez-hGsuU").attr("target", "_blank");
 }
 
 function setWinter()  {
   $("body").css("background-image", "url('images/winterblur.jpg')");
-  $("#winpic").attr("src", "images/winter.jpg");
+  $("img").attr("src", "images/winter.jpg",);
+  $("h1").text("Winter");
+  $("a").attr("href", "https://www.youtube.com/watch?v=U3O7Xb2bgsY").attr("target", "_blank");
 }
